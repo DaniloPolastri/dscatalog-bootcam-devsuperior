2280fb4a6e067a8deea5d0b5b8bb72ef6eb6c234
DaniloPolastri <danilopolastri.ti@gmail.com>
2021-01-21 19:22:36 -0300

    Database Seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- Side da base de dados (Semear os dados da uma carga inicial no banco) = import.sql
+INSERT INTO tb_category (name) values ('Books');
+INSERT INTO tb_category (name) values ('Electronics');
+INSERT INTO tb_category (name) values ('Computes');

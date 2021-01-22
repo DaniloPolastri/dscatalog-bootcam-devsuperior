288919e9b876c2f08173d27b0f35f58d9d75e2e4
DaniloPolastri <danilopolastri.ti@gmail.com>
2021-01-22 19:16:29 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +18,22 @@ public class CategoryService {
     private CategoryRepository respository;
 
     @Transactional(readOnly = true) // evitar q eu faco lock no banco de dados e melhorar a perfomace, operacoes somente letiura lembrar de por readOnly = true
-    public List<Category> findAll(){
-        return respository.findAll();
+    public List<CategoryDTO> findAll(){
+
+        List<Category> list = respository.findAll();
+
+        // converter a lista normal para stream que permite trabalhar com funcoes de alta ordem por exemplo lambda
+        // Map = ela transforma cada elemento original em uma outra coisa ela aplica uma funcao a cada elemento da sua lista
+        // Collect = transforma uma stream novamente para uma lista
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+        /*List<CategoryDTO> listDto = new ArrayList<>();
+
+        for(Category cat : list){
+            listDto.add(new CategoryDTO(cat));
+        }
+
+        return listDto;*/
+
     }
 }

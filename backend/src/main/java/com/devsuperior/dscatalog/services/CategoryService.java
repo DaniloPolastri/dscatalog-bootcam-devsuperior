8098ee45f838b6a17bf11126c0e4470ad43ce9d8
DaniloPolastri <danilopolastri.ti@gmail.com>
2021-01-25 20:37:36 -0300

    (PUT)Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,12 +3,12 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -44,12 +44,12 @@ public class CategoryService {
     public CategoryDTO findById(Long id) {
         // Optional e para evitar de voce trabalhar com o valor nulo
         Optional<Category> obj = respository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 
         return new CategoryDTO(entity);
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public CategoryDTO insert(CategoryDTO dto) {
         Category entity = new Category();
         entity.setName(dto.getName());
@@ -57,5 +57,19 @@ public class CategoryService {
 
         return  new CategoryDTO(entity);
 
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        /*o findbyid ele efetiva o acesso ao banco de dados, e o getOne ele nao toca no banco de dados vai instancia um obejto provisorio com o esse id naquele objeto
+        so quando vc manda salvar ai sim ele vai no banco de dados, a intencao do getOne e para nao ir no banco sem necessidade*/
+        try {
+            Category entity = respository.getOne(id);
+            entity.setName(dto.getName());
+            entity = respository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw  new ResourceNotFoundException("Id not Found " + id);
+        }
     }
 }

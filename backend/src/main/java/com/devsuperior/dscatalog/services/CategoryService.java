6e5bb8d972719f8bce775daaf1011f6bc3dbb2fa
DaniloPolastri <danilopolastri.ti@gmail.com>
2021-01-25 20:22:59 -0300

    (Post)Insert category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -48,4 +48,14 @@ public class CategoryService {
 
         return new CategoryDTO(entity);
     }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = respository.save(entity);
+
+        return  new CategoryDTO(entity);
+
+    }
 }

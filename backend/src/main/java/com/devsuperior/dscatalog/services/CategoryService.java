847d9b3f431f9fe1c09e50902169446c7ae18be4
DaniloPolastri <danilopolastri.ti@gmail.com>
2021-01-25 20:12:27 -0300

    Find Category id, exception Handling

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,11 +22,12 @@ public class CategoryService {
     @Transactional(readOnly = true) // evitar q eu faco lock no banco de dados e melhorar a perfomace, operacoes somente letiura lembrar de por readOnly = true
     public List<CategoryDTO> findAll(){
 
-        List<Category> list = respository.findAll();
+
 
         // converter a lista normal para stream que permite trabalhar com funcoes de alta ordem por exemplo lambda
         // Map = ela transforma cada elemento original em uma outra coisa ela aplica uma funcao a cada elemento da sua lista
         // Collect = transforma uma stream novamente para uma lista
+        List<Category> list = respository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
         /*List<CategoryDTO> listDto = new ArrayList<>();
@@ -42,7 +44,7 @@ public class CategoryService {
     public CategoryDTO findById(Long id) {
         // Optional e para evitar de voce trabalhar com o valor nulo
         Optional<Category> obj = respository.findById(id);
-        Category entity = obj.get();
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 
         return new CategoryDTO(entity);
     }

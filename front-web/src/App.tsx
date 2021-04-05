18a3fe5589f96b28e6fd9ccd3e096b9f743ee9cd
Danilo <danilopolastri.ti@gmail.com>
2021-04-05 18:51:26 -0300

    Add front-web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const App = () => {
+
+    /*
+    * counter e o valor do estado
+    * setcounter - funcao que vai alterar o estado
+    * */
+
+    return (
+        <>
+            <h1>App</h1>
+        </>
+    );
+}
+
+export default App;

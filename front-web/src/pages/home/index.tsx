9712d93a989560b94950496af5a3b30f276c117f
Danilo <danilopolastri.ti@gmail.com>
2021-04-06 19:41:29 -0300

    design page home

--- front-web/src/pages/home/index.tsx
+++ front-web/src/pages/home/index.tsx
@@ -1,6 +1,23 @@
+import React from "react";
+import {ReactComponent as MainImage} from '../../core/assets/img/Desenho.svg'
+import './styles.scss'
+import ButtonIcon from "../../core/components/ButtonIcon";
+import {Link} from 'react-router-dom'
+
 const Home = () => (
-    <div>
-        <h1>Home</h1>
+    <div className="home-container">
+        <div className="row home-content">
+            <div className="col-6">
+                <h1 className="text-title">Conheça o melhor<br/> catálogo de produtos</h1>
+                <p className="text-subtitle">Ajudaremos vôce a encontrar os melhores<br/> produtos disponíveis no mercado</p>
+                <Link to="/catalog">
+                    <ButtonIcon text="inicie agora a sua busca"/>
+                </Link>
+            </div>
+            <div className="col-6">
+                <MainImage className="main-image"/>
+            </div>
+        </div>
     </div>
 );
 

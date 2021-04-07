b66aaccd9a1e9fcf72b3ecb1d772f9465dea7886
Danilo <danilopolastri.ti@gmail.com>
2021-04-07 19:48:11 -0300

    Finish product listing layout

--- front-web/src/pages/catalog/index.tsx
+++ front-web/src/pages/catalog/index.tsx
@@ -1,11 +1,23 @@
 import './styles.scss';
+import ProductCard from "./components/ProductCard";
 
 const Catalog = () => (
     <div className="catalog-container">
         <h1 className="catalog-title">Catálago de produtos</h1>
 
         <div className="catalog-products">
-            teste
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
         </div>
     </div>
 );

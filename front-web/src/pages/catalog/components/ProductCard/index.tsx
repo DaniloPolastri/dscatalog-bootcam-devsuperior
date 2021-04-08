6b5aafd2edf3f2d76f32cee09e33f5151542bec3
Danilo <danilopolastri.ti@gmail.com>
2021-04-08 20:27:55 -0300

    Finish page product details

--- front-web/src/pages/catalog/components/ProductCard/index.tsx
+++ front-web/src/pages/catalog/components/ProductCard/index.tsx
@@ -1,15 +1,14 @@
 import './styles.scss'
 import {ReactComponent as ProductImage} from '../../../../core/assets/img/product.svg'
+import ProductPrice from "../../../../core/components/ProductPrice/ProductPrice";
+
 
 const ProductCard = () => (
     <div className="card-base border-radius-10 product-card">
        <ProductImage/>
        <div className="product-info">
            <h6 className="product-name">Computador Desktop - Intel Core i7</h6>
-           <div className="product-price-container">
-               <span className="product-currency">R$</span>
-               <h3 className="product-price">2.779,00</h3>
-           </div>
+           <ProductPrice price="2.778,00"/>
        </div>
 
     </div>
